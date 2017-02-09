c9f199da956b73cdca9a7dbbee7154ead93d81a3
katestrykermcm <katestrykermcm@gmail.com>
2017-02-08 21:52:39 -0500

    completed hw2

--- hw2/script.js
+++ hw2/script.js
@@ -9,28 +9,124 @@ var allWorldCupData;
 function updateBarChart(selectedDimension) {
 
     var svgBounds = d3.select("#barChart").node().getBoundingClientRect(),
-        xAxisWidth = 100,
-        yAxisHeight = 70;
+        // add padding on all sides
+        padding = 80;
+        // the height and width of the actual drawing area
+        xAxisWidth = svgBounds.width - padding * 2;
+        yAxisHeight = svgBounds.height - padding * 2;
 
-    // ******* TODO: PART I *******
+        
 
-    // Create the x and y scales; make
-    // sure to leave room for the axes
+    // Scale for X axis
+    var minYear = d3.min(allWorldCupData, function(d){
+        return d.year;
+    });
+    var maxYear = d3.max(allWorldCupData, function(d){
+        return d.year;
+    });
+    // x : 1930 - 2014
+    var xScale = d3.scaleLinear()
+            // i think this is where to start the count w.r.t data
+            .domain([minYear, maxYear])
+            // i think this is x value to place at
+            .range([0, xAxisWidth]); 
+            //.nice();
+    // alternative option to explore
+    // var xScale = d3.scaleBand().range([0, xAxisWidth]).padding(.1);        
+
+
+    // Create X axis
+    var xContainer = d3.select("#xAxis");
+    // get year tick values from data 
+    var xValues = allWorldCupData.map(function (d) {
+        return d.year;
+    });
+    // add missing years
+    xValues.push(1942);
+    xValues.push(1946);
+    var xAxis = d3.axisBottom()
+        .tickValues(xValues)
+        // removes commas from the years ex. 2,000 -> 2000
+        .tickFormat(d3.format("d"));
+    // assign the scale to the axis
+    xAxis.scale(xScale);
+    xContainer.append("g")
+        .attr("transform", "translate(" + (padding + ((xAxisWidth / allWorldCupData.length)/2)) + ", " + (padding + yAxisHeight) + ")")
+        .call(xAxis)
+        .selectAll("text")  
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", "rotate(-65)");
+
+
+    // Scale for Y axis
+    var maxY = d3.max(allWorldCupData, function(d){
+        return d[selectedDimension];
+    });
+    var yScale = d3.scaleLinear()
+        .domain([0, maxY])
+        .range([yAxisHeight, 0]);
+
+
+    // Create Y axis
+    var yContainer = d3.select("#yAxis");
+    yContainer = yContainer.enter()
+        .append("g")
+        .merge(yContainer);
+    yContainer.exit().remove();
+    yContainer
+        // moving the axis to the right place
+        .transition().attr("transform", "translate(" + padding + ", " + padding + ")")
+        .call(d3.axisLeft().scale(yScale));
+
+
 
     // Create colorScale
+    var colorScale = d3.scaleLinear().domain([0, maxY])
+        .range(["yellow", "red"]);
 
-    // Create the axes (hint: use #xAxis and #yAxis)
 
-    // Create the bars (hint: use #bars)
-
-    // ******* TODO: PART II *******
-
-    // Implement how the bars respond to click events
-    // Color the selected bar to indicate is has been selected.
-    // Make sure only the selected bar has this new color.
-
-    // Output the selected bar to the console using console.log()
-
+    // Create the bars
+    // Select all rect's in #bars and bind the world cup data to them
+    var bars = d3.select("#bars").selectAll("rect").data(allWorldCupData);
+    // handle the enter() condition and merge with existing rects
+    bars = bars.enter()
+            .append('rect')
+            .merge(bars);
+    // handle the exit() case to remove any bars that no longer have data assigned to them
+        bars.exit().remove();
+     // finally, assign the necessary attributes to the bars   
+        bars
+            // new: we add the padding via a tranform/translate
+            .attr("transform", "translate(" + padding + "," + padding + ")")
+            // Implement how the bars respond to click events
+            .on("click", function() {
+                // Make sure only the selected bar has this new color
+                d3.select("#bars").selectAll("rect").style('fill', function (d) {
+                        return  colorScale(d[selectedDimension]);
+                    });
+                // Color the selected bar to indicate is has been selected
+                d3.select(this).style("fill", "purple");
+                // Output the selected bar to the console using console.log()
+                console.log(d3.select(this));
+                d3.event.stopPropagation();
+            })
+            .transition().attr('x', function (d) { // starting x point for individual bars
+                return  xScale(d.year);
+            })
+            .attr('width', function (d) { // width of individual bars
+                return  (xAxisWidth / allWorldCupData.length);
+            })
+            .attr('y', function (d) {
+                return yScale(d[selectedDimension]);
+            })
+            .attr('height', function (d) {
+                return  yAxisHeight - yScale(d[selectedDimension]);
+            })
+            .attr('fill', function (d) {
+                return  colorScale(d[selectedDimension]);
+            });
 }
 
 /**
@@ -40,11 +136,12 @@ function updateBarChart(selectedDimension) {
  *  goals, matches, attendance and teams.
  */
 function chooseData() {
-
-    // ******* TODO: PART I *******
-    //Changed the selected data when a user selects a different
-    // menu item from the drop down.
-
+    // clear data about previous dimensions in the axes
+    //d3.selectAll("g").html("");
+    // Changed the selected data when a user selects a different menu item from the drop down
+    var selectMenu = document.getElementById("dataset");
+    var newDimension = selectMenu.options[selectMenu.selectedIndex].value;
+    updateBarChart(newDimension);
 }
 
 /* DATA LOADING */
@@ -78,5 +175,34 @@ d3.csv("data/fifa-world-cup.csv", function (error, csv) {
     // Store csv data in a global variable
     allWorldCupData = csv;
     // Draw the Bar chart for the first time
-    updateBarChart('attendance');
+    updateBarChart("attendance");
 });
+
+
+
+ 
+
+    // bar animation
+
+
+// Part II: Selecting a Word Cup
+
+// In the updateBarChart() function, there is one location flagged with
+
+// // ******* TODO: PART II*******
+// Here, you should make your bars respond to click events. This involves highlighting the selected bar with a different color, and outputting the selected bar to the javascript console using console.log().
+
+// Grading
+
+// The rubrics on the assignment are:
+
+// 75%: Part I: Bar chart shows current selection, with appropriate scales, axes, and coloring. Switching between the different attributes works.
+// 25%: Part II: Bars can be selected and are highlighted, and selection outputs the selected bar to the javascript console.
+
+
+
+
+
+
+
+
